b9c7a3cc32f793042fc1209cc8ac4259f6e35501
adiaz2308 <adiaz@canal6.com.ni>
2016-01-26 18:04:02 -0600

    Ya se puede ver el mapa

---
+++ app1/maps.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var map = {
+		center: new google.maps.LatLng(12.1214925,-86.2454143),
+		zoom: 16
+	};
+	var map1 = new google.maps.Map(document.getElementById("mapa"), map);
+})
